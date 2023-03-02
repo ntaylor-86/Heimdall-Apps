9a0486675b4facf31ca340cc1bf24421fa6ef20a
ntaylor-86 <ntaylor0377@gmail.com>
2023-03-02 23:45:00 +1000

    Updated the styling

--- openmediavault/config.blade.php
+++ openmediavault/config.blade.php
@@ -1,5 +1,5 @@
 <h2>{{ __('app.apps.config') }} ({{ __('app.optional') }}) @include('items.enable')</h2>
-<div class="items">
+<div class="items" style="flex-wrap: wrap;">
     <div class="input">
         <label>{{ strtoupper(__('app.url')) }}</label>
         {!! Form::text('config[override_url]', isset($item) ? $item->getconfig()->override_url : null, ['placeholder' => __('app.apps.override'), 'id' => 'override_url', 'class' => 'form-control']) !!}
